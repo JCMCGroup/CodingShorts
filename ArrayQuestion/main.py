1869de29f61b9318afd461cf53d2c7855e040fdb
JCMCGroup-Joshua <joshua.akester@jcmcgroup.co.uk>
2023-07-25 17:55:49 +0100

    Episode 1 - Arrays can do what...

---
+++ ArrayQuestion/main.py
@@ -0,0 +1,13 @@
+
+def main():
+    # Create an array of integers
+    int_array = [1, 2, 3, 4, 5]
+
+    Last_element = int_array[len(int_array)-1]
+    print("Last Item:",Last_element)
+    last_element = int_array[-1]
+    print("Last Item:",last_element)
+
+
+if __name__ == "__main__":
+    main()
